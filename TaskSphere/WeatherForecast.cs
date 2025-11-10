a1b5d1a6ab4a4a031a0eea622a7e0d629912639f
Sherif Ashraf <sherifashrefrefaat@outlook.com>
2025-11-11 00:41:00 +0200

    finsih edits

--- TaskSphere/WeatherForecast.cs
+++ TaskSphere/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace TaskSphere
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 5000 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
